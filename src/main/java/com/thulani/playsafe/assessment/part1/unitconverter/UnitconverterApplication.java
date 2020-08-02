98e60a1037e552521a0245b9b3adcdce1b4fcee7
thue101 <56626050+thue101@users.noreply.github.com>
2020-08-02 17:01:05 +0200

    Agile Story 1: This is the springboot shell with the necessary dependencies for Playsafe Assessment project part 1 which is  "unitConverter"

---
+++ src/main/java/com/thulani/playsafe/assessment/part1/unitconverter/UnitconverterApplication.java
@@ -0,0 +1,13 @@
+package com.thulani.playsafe.assessment.part1.unitconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitconverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitconverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thulani/playsafe/assessment/part1/unitconverter/UnitconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thulani.playsafe.assessment.part1.unitconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
